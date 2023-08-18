aa3eadd18a85f7758b5f1f7f2fbf7c57d3419c71
Carlos Ortiz <bitsplit@gmail.com>
2023-08-17 21:38:52 -0400

    Add command line parser and new formula.

--- README.md
+++ README.md
@@ -1,2 +1,22 @@
-# piday
-Celebrating Pi Day with Python
+# Pi Day
+
+Celebrating Pi Day (March 14) with Python.
+
+## Changes
+
+### 2023-03-15
+- Initial implementation using naive Bailey-Borwein-Plouffe formula.
+
+### 2023-06-06
+- Add implementation using Machin's formula.
+
+### 2023-08-17
+- Add command line argument parser with parameters for width (default 80),
+height (default 24), source image (default pi.png), inverted colors (default 
+False), and keeping the source image's aspect ratio (default False).
+- Fix bug in Machin's formula implementation.
+- Add command line parameter for raw output, with width parameter specifying the
+number of decimal digits.
+- Refactor to make formulas for computing pi modular.
+- Add command line parameter to specify formula.
+- Add command line parameters to test formulas, all formulas or specific ones.
--- piday2023.py
+++ piday2023.py
@@ -1,54 +1,131 @@
+import argparse
+import contextlib
 import sys
-from decimal import Decimal, localcontext, ROUND_DOWN
+import traceback
+from decimal import Decimal, localcontext
 
-import PIL.Image
+from PIL import Image
+
+from typing import Type, Callable, TextIO
 
 
-DEFAULT_LINE_WIDTH: int = 80
+DEFAULT_PRECISION: int = 100
+DEFAULT_TEXT_WIDTH: int = 80
+DEFAULT_TEXT_HEIGHT: int = 24
+DEFAULT_SOURCE_IMAGE: str = 'pi.png'
+
+Formula = Type[Callable[[int], str]]
 
 
-def compute_pi(precision: int = 100) -> str:
-    """Compute π to the specified precision."""
+def compute_pi(precision: int = DEFAULT_PRECISION,
+               formula: Formula = None) -> str:
+    """Compute π to the specified decimal precision with the given formula."""
+    
+    if formula is None:
+        formula = compute_pi_agm
+
     original_max_str_digits = sys.get_int_max_str_digits()
-    rounding_precision = precision + 2
-    conversion_precision = precision * precision
+    conversion_precision = precision*2
+    changed_max_str_digits: bool = False
     try:
-        sys.set_int_max_str_digits(conversion_precision)
-        with localcontext() as ctx:
-            ctx.prec = rounding_precision
-            ctx.rounding = ROUND_DOWN
-            return compute_pi_bbp(precision)
-            # return compute_pi_machin(precision)
+        with contextlib.suppress(ValueError):        
+            sys.set_int_max_str_digits(conversion_precision)
+            changed_max_str_digits = True
+        return formula(precision)
     finally:
-        sys.set_int_max_str_digits(original_max_str_digits)
+        if changed_max_str_digits:
+            with contextlib.suppress(ValueError):        
+                sys.set_int_max_str_digits(original_max_str_digits)
 
 
-def compute_pi_bbp(precision: int = 100) -> str:
-    """Compute π to the specified precision using the Bailey-Borwein-Plouffe formula."""
-    pi = Decimal(0)
-    precision_scale: int = 10**(precision)
-    pi_int: int = 0
-    old_pi_int: int = None
-    for k in range(precision):
-        _8k = 8*k
-        old_pi_int = pi_int
-        term1 = Decimal(4) / Decimal(_8k+1)
-        term2 = Decimal(2) / Decimal(_8k+4)
-        term3 = Decimal(1) / Decimal(_8k+5)
-        term4 = Decimal(1) / Decimal(_8k+6)
-        term = term1 - term2 - term3 - term4
-        denominator = Decimal(16**k)
-        pi += term / denominator
+def compute_pi_bbp(precision: int = DEFAULT_PRECISION) -> str:
+    """Compute π to the specified decimal precision using the BBP formula."""
+    precision_padding: int = 4
+    
+    with localcontext() as ctx:
+        ctx.prec = precision + precision_padding
+
+        pi = Decimal(0)
+        precision_scale: int = 10**(precision+precision_padding)
+        pi_int: int = 0
+        old_pi_int: int = None
+        for k in range(precision+precision_padding):
+            _8k = 8*k
+            old_pi_int = pi_int
+            term1 = Decimal(4) / Decimal(_8k+1)
+            term2 = Decimal(2) / Decimal(_8k+4)
+            term3 = Decimal(1) / Decimal(_8k+5)
+            term4 = Decimal(1) / Decimal(_8k+6)
+            term = term1 - term2 - term3 - term4
+            denominator = Decimal(16**k)
+            pi += term / denominator
+            pi_int = int(pi*precision_scale)
+            if pi_int == old_pi_int:
+                break
+        pi_str = str(pi_int)
+                    
+        return f'{pi_str[0]}.{pi_str[1:]}'[:precision+2]
+
+
+def compute_pi_machin(precision: int = DEFAULT_PRECISION) -> str:
+    """Compute π to the specified decimal precision using Machin's formula."""
+    precision_padding: int = 4
+    with localcontext() as ctx:
+        ctx.prec = precision + precision_padding
+        
+        pi: Decimal = 4 * (4 * decimal_atan2(Decimal(5)) - decimal_atan2(Decimal(239)))
+        precision_scale: int = 10**(precision + 2)
+        pi_int: int = 0
         pi_int = int(pi*precision_scale)
-        if pi_int == old_pi_int:
-            break
-    pi_str = str(pi_int)
-                
-    return f'{pi_str[0]}.{pi_str[1:]}'
+        pi_str: str = str(pi_int)[:precision + 1]
+        assert len(pi_str) == precision + 1
+        return f'{pi_str[0]}.{pi_str[1:]}'
 
 
+def compute_pi_agm(precision: int = DEFAULT_PRECISION) -> str:
+    """Compute π to the specified decimal precision using the AGM formula."""
+    precision_padding: int = 3
+    with localcontext() as ctx:
+        ctx.prec = precision + precision_padding
+        epsilon = Decimal(10)**(-precision) / Decimal(2)
+        
+        a = n = Decimal(1)
+        g = 1 / Decimal(2).sqrt()
+        z = Decimal('0.25')
+        half = Decimal('0.5')
+        old_pi, pi = None, 0
+        
+        def iterate():
+            nonlocal a, g, z, n, old_pi, pi
+            x = [(a + g) * half, (a * g).sqrt()]
+            var = x[0] - a
+            z -= var * var * n
+            n += n
+            a, g = x
+            old_pi, pi = pi, a * a / z
+        
+        for i in range(18):
+            iterate()
+        
+        while old_pi - pi > epsilon:
+            iterate()
+        
+        return str(pi)[:precision+2]
+
+        
 def decimal_atan2(y: Decimal, x: Decimal = None) -> Decimal:
-    """Compute the arctangent of y/x, if x > 0."""
+    """Compute the arctangent of y/x, if x > 0, or of 1/y if x is None.
+    
+    Sums terms of the power series expansion of arctangent.
+    
+    Args:
+        y: The Decimal numerator of the fraction if x is not None, or the
+           denominator otherwise.
+        x: The Decimal denominator of the fraction if not None.
+    
+    Returns:
+        The Decimal arctangent of y/x, if x > 0, or of 1/y if x is None.
+    """
     
     if x is None:
         y, x = Decimal(1), y
@@ -76,33 +153,30 @@ def decimal_atan2(y: Decimal, x: Decimal = None) -> Decimal:
     return total
 
 
-def compute_pi_machin(precision: int = 100) -> str:
-    """Compute π to the specified precision using Machin's formula."""
-    pi: Decimal = 4 * (4 * decimal_atan2(Decimal(5) - decimal_atan2(Decimal(239))))
-    precision_scale: int = 10**(precision + 2)
-    pi_int: int = 0
-    pi_int = int(pi*precision_scale)
-    pi_str: str = str(pi_int)[:precision + 1]
-    assert len(pi_str) == precision + 1
-    return f'{pi_str[0]}.{pi_str[1:]}'
-
-
-def pi_ascii_art(width: int = None, *, inverted: bool = True) -> str:
+def pi_ascii_art(width: int = None,
+                 height: int = None,
+                 source_image: str = None,
+                 inverted: bool = False,
+                 keep_aspect_ratio: bool = False) -> str:
     """Format π as ASCII art."""
+    
     if width is None:
-        width = DEFAULT_LINE_WIDTH
-
+        width = DEFAULT_TEXT_WIDTH
+    if height is None:
+        height = DEFAULT_TEXT_HEIGHT
+    if source_image is None:
+        source_image = DEFAULT_SOURCE_IMAGE
+    
     def need_digit(pixel: int) -> int:
-        return 1 if pixel and not inverted or not pixel and inverted else 0
+        return 1 if pixel and inverted or not (pixel or inverted) else 0
     
     lines: list[str] = []
     line: list[str] = []
-    with PIL.Image.open('pi.png') as image:
+    
+    with Image.open(source_image) as image:
         image = image.convert('L')
-        image_width, image_height = image.size
-        aspect_ratio = image_width / image_height
-        height = int(width / aspect_ratio)
-        image = image.resize((width, height))
+        image = resize_image(image, width, height,
+                             keep_aspect_ratio=keep_aspect_ratio)
         image = image.convert('1')
         pixels = image.getdata()
         num_digits: int = sum(map(need_digit, pixels))
@@ -122,19 +196,150 @@ def pi_ascii_art(width: int = None, *, inverted: bool = True) -> str:
                 lines.append(''.join(line))
                 line = []
                 num_chars = 0
-    return '\n'.join(lines)
+    return '\n'.join(line for line in lines if line.strip())
             
+
+def resize_image(image: Image,
+                 width: int, height: int = None, *,
+                 keep_aspect_ratio: bool = True) -> Image:
+        
+    image_width, image_height = image.size
+    image_aspect_ratio = image_width / image_height
+
+    if height is None:
+        height = int(width / image_aspect_ratio)
+
+    if keep_aspect_ratio:
+        aspect_ratio = image_aspect_ratio
+    else:
+        aspect_ratio = width / height
+    
+    if image_aspect_ratio < aspect_ratio:
+        height = int(width / image_aspect_ratio)
+    
+    resized_image = image.resize((width, height), Image.LANCZOS)
+    return resized_image
+
 
 def main():
     width: int
-    if sys.argv[1:]:
-        width = int(sys.argv[1])
-    else:
-        width = DEFAULT_LINE_WIDTH
-    
-    print(pi_ascii_art(width))
 
+    success_code: int = 0
+    failure_code: int = 1
+    
+    exit_code: int = failure_code
+    
+    try:
+        opts = get_script_options()
+        
+        if opts.test_formulas:
+            test_formulas()
+            exit_code = success_code
+        elif opts.test_formula is not None:
+            formula: Formula = globals()[opts.test_formula]
+            test_formula(formula)
+            exit_code = success_code
+        else:
+            print_pi(opts)
+            
+    except Exception as e:
+        print_exception(e)
+    
     return 0
+
+
+def get_script_options() -> argparse.Namespace:
+    """Get the command line options for this script."""
+    
+    parser = argparse.ArgumentParser(description='Print π to the specified width and height.')
+    
+    parser.add_argument('-w', '--width',
+                        type=int, default=DEFAULT_TEXT_WIDTH,
+                        help='The width of the output.')
+    parser.add_argument('-l', '--height', '--lines',
+                        type=int, default=DEFAULT_TEXT_HEIGHT,
+                        help='The height of the output.')
+    parser.add_argument('-s', '--source-image',
+                        type=str, default=DEFAULT_SOURCE_IMAGE,
+                        help='The source image to use.')
+    parser.add_argument('-i', '--inverted',
+                        action='store_true', default=False,
+                        help='Invert the colors.')
+    parser.add_argument('-k', '--keep-aspect-ratio',
+                        action='store_true', default=False,
+                        help='Keep the aspect ratio of the source image.')
+    parser.add_argument('-r', '--raw',
+                        action='store_true', default=False,
+                        help='Print the raw digits of π.  Width specifies '
+                             'number of digits.')
+    parser.add_argument('--test-formulas',
+                        action='store_true', default=False,
+                        help='Test the formulas for computing pi.')
+    parser.add_argument('--test-formula',
+                        type=str, default=None,
+                        help='Test the specified formula for computing pi.')
+
+    opts = parser.parse_args()
+    return opts
+
+
+def print_pi(opts: argparse.Namespace) -> None:
+    """Print π to the specified width and height."""
+    width: int
+    height: int
+    width, height = opts.width, opts.height
+    
+    source_image: str = opts.source_image
+    
+    inverted: bool = opts.inverted
+    keep_aspect_ratio: bool = opts.keep_aspect_ratio
+    
+    raw: bool = opts.raw
+    
+    if not raw:
+        print(pi_ascii_art(width, height, source_image, inverted,
+                           keep_aspect_ratio))
+    else:
+        print(compute_pi(width))
+
+
+def print_exception(e: Exception, *,
+                    message: str = None,
+                    file: TextIO = sys.stderr) -> None:
+
+    stack_trace = traceback.format_exc()
+    error_message = f"{message}: {e}"
+    error_text = f"{error_message}\nStack Trace:\n{stack_trace}"
+    print(error_text, file=file)
+
+
+def test_formulas() -> None:
+    """Test the formulas."""
+    
+    formulas = [
+        compute_pi_bbp,
+        compute_pi_machin,
+        compute_pi_agm,
+    ]
+
+    for formula in formulas:
+        test_formula(formula)
+
+
+def test_formula(formula: Formula) -> None:
+    expected_last_10_digits = {
+        10**1: '1415926535',
+        10**2: '3421170679',
+        10**3: '2164201989',
+        10**4: '5256375678',
+        10**5: '5493624646',
+    }
+    
+    for precision, expected in expected_last_10_digits.items():
+        pi = compute_pi(precision, formula)
+        actual = pi[-10:]
+        assert actual == expected, f'Expected {expected}, got {actual} for {formula=} and {precision=}.'
+        print(f'Formula {formula.__name__} passed for precision {precision}.')
 
 
 if __name__ == '__main__':
